011852b0e0b9a70c705d5fb921f80eee02082e39
Jay's <dallardj@gmail.com>
2024-01-26 03:04:50 +0100

    fix calendar seeders

--- src/database/seeds/CalendarSettingsTableSeeder.php
+++ src/database/seeds/CalendarSettingsTableSeeder.php
@@ -13,19 +13,25 @@ class CalendarSettingsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        setting([
-            'kassie.calendar.slack_emoji_importance_full',
-            ':full_moon_with_face:',
-        ], true);
+        if (is_null(setting('kassie.calendar.slack_emoji_importance_full', true))) {
+            setting([
+                'kassie.calendar.slack_emoji_importance_full',
+                ':full_moon_with_face:',
+            ], true);
+        }
 
-        setting([
-            'kassie.calendar.slack_emoji_importance_half',
-            ':last_quarter_moon:',
-        ], true);
+        if (is_null(setting('kassie.calendar.slack_emoji_importance_half', true))) {
+            setting([
+                'kassie.calendar.slack_emoji_importance_half',
+                ':last_quarter_moon:',
+            ], true);
+        }
 
-        setting([
-            'kassie.calendar.slack_emoji_importance_empty',
-            ':new_moon_with_face:',
-        ], true);
+        if (is_null(setting('kassie.calendar.slack_emoji_importance_empty', true))) {
+            setting([
+                'kassie.calendar.slack_emoji_importance_empty',
+                ':new_moon_with_face:',
+            ], true);
+        }
     }
 }
